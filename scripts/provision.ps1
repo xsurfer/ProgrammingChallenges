a16def468ebc575faac49eed2f180d0f0c4c02d4
Fabio Perfetti <perfetti.f@pg.com>
2022-10-04 01:02:33 +0200

    add provision script

---
+++ scripts/provision.ps1
@@ -0,0 +1,47 @@
+$prefix = Read-Host "Please enter a unique prefix for the resources"
+$subscription = "PG-Sandbox-01"
+$location = "eastus2"
+$resourceGroup = "AZ-RG-perfettif-Sandbox-01"
+$storageAccountName = -join($prefix, "sa")
+$servicePlanName = -join($prefix, "plan")
+$functionAppName = -join($prefix, "app")
+$vmName = -join($prefix, "vm")
+$vmImage = "MicrosoftVisualStudio:visualstudio2019latest:vs-2019-ent-latest-ws2019:2022.07.15"
+
+
+$subscriptions = az login
+
+$subscription = az account set `
+    --subscription $subscription
+
+$storageAccount = az storage account create `
+    --name $storageAccountName `
+    --resource-group $resourceGroup `
+    --location $location `
+    --sku Standard_LRS       
+
+#az storage account keys list -g $resourceGroup -n $storageAccountName
+
+$appServicePlan = az appservice plan create `
+    -g $resourceGroup `
+    -n $servicePlanName `
+    --sku S1 `
+    --location $location
+
+$functionApp = az functionapp create `
+    -g $resourceGroup `
+    -p $servicePlanName `
+    -n $functionAppName `
+    -s $storageAccountName `
+    --os-type Windows `
+    --functions-version 3
+
+$vm = az vm create `
+    -n $vmName `
+    -g $resourceGroup `
+    -l $location `
+    --image $vmImage `
+    --public-ip-sku Standard `
+    --nsg-rule RDP `
+    --authentication-type password `
+    --admin-username pgcandidate
