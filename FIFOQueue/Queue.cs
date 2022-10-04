ce65f7bd9e07bb9f737e865e14a53ad827c7f37b
Fabio Perfetti <perfetti.f@pg.com>
2022-10-04 02:07:35 +0200

    rename solution and add FIFOQueue challenge

---
+++ FIFOQueue/Queue.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FIFOQueue
+{
+    public class Queue<T> : IQueue<T>
+    {
+        private int _counter = 1;
+
+        public Queue(int size)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool add(T element)
+        {            
+            throw new NotImplementedException();
+        }
+
+        public T poll()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
