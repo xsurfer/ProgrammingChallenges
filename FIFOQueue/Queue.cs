1c72d1358f2dd28c334a2c21522a53d8867ae203
Fabio Perfetti <perfetti.f@pg.com>
2022-10-04 18:06:38 +0200

    Revert "Candidate sol (#15)"
    This reverts commit 9c5ee70d316b01dec5bcc037ea5d6675effb5e8b.

--- FIFOQueue/Queue.cs
+++ FIFOQueue/Queue.cs
@@ -10,53 +10,20 @@ namespace FIFOQueue
 {
     public class Queue : IQueue
     {
-        private int[] ele;
-        private int front;
-        private int rear;
-        private int max;
-        private int count;
-
+        
         public Queue(int size)
         {
-            ele = new int[size];
-            front = 0;
-            rear = -1;
-            max = size;
-            count = 0;
+            throw new NotImplementedException();
         }
 
         public bool add(int element)
         {
-            if (count == max)
-            {                
-                throw new InvalidOperationException();                
-            }
-            else
-            {
-                rear = (rear + 1) % max;
-                ele[rear] = element;
-
-                count++;
-            }
-            return true;
+            throw new NotImplementedException();
         }
 
         public int poll()
         {
-            if (count == 0)
-            {                
-                throw new InvalidOperationException();
-            }
-            else
-            {
-                int temp = ele[front];
-                Console.WriteLine("deleted element is: " + ele[front]);
-
-                front = (front + 1) % max;
-
-                count--;
-                return temp;
-            }
+            throw new NotImplementedException();
         }
 
     }
